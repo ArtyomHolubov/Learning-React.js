7e6b5462cab9cde88fc5979ab9543d88f866df3c
ArtyomHolubov <artyom.holubov@gmail.com>
2020-09-16 20:10:36 +0300

    Added HomeWorkDescription component

--- src/hw1/components/HomeWork1.js
+++ src/hw1/components/HomeWork1.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ProductTable from './ProductTable';
+import HomeWorkDescription from './HomeWorkDescription';
+import Descriptions from '../../data/hw.json';
 
 function HomeWork1() {
   return (
     <div id="hw1">
       <h1>Home Work 1</h1>
+      <HomeWorkDescription data={Descriptions.hw1.descriprion}></HomeWorkDescription>
       <h3>The goods presented in the table are taken from here <a href="https://rozetka.com.ua/phones-mp3-gps/c80257/sort=rank/" target="_blank" rel="noopener noreferrer">https://rozetka.com.ua/phones-mp3-gps/c80257/sort=rank/</a></h3>
       <ProductTable />
     </div>
---
+++ src/hw1/components/HomeWorkDescription.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import {
+    Button,
+    Header,
+    Segment,
+    TransitionablePortal,
+} from 'semantic-ui-react'
+
+export default class HomeWorkDescription extends Component {
+    state = {
+        open: false
+    }
+
+    handleOpen = () => this.setState({ open: true })
+
+    handleClose = () => this.setState({ open: false })
+
+    render() {
+        const { open } = this.state
+
+        return (
+            <TransitionablePortal
+                closeOnTriggerClick
+                onOpen={this.handleOpen}
+                onClose={this.handleClose}
+                openOnTriggerClick
+                trigger={
+                    <Button
+                        content={open ? 'Close Description' : 'Open Description'}
+                        negative={open}
+                        positive={!open}
+                    />
+                }
+            >
+                <div className="hw-description" dangerouslySetInnerHTML={{ __html: this.props.data }} />
+            </TransitionablePortal>
+        )
+    }
+}
