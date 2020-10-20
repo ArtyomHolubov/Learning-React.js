1effc012b2571af61bdd6b6a44981b21b38d30b5
ArtyomHolubov <artyom.holubov@gmail.com>
2020-10-21 01:24:08 +0300

    Added cart to localStorage

--- src/hw6/redux/reducers/cart.js
+++ src/hw6/redux/reducers/cart.js
@@ -1,23 +1,33 @@
 import { ADD_TO_CART } from "../actions/cart";
 import { REMOVE_FROM_CART } from "../actions/cart";
 
+const key = 'cart';
+const item = localStorage.getItem(key);
+const cartFromStorage = item ? JSON.parse(item) : []
 
-export default (state = [], action) => {
+export default (state = cartFromStorage, action) => {
     switch (action.type) {
-        case ADD_TO_CART: return [...state, {
-            id: action.payload.id
-        }];
+        case ADD_TO_CART: {
+            const resultState = [...state, {
+                id: action.payload.id
+            }];
+            localStorage.setItem(key, JSON.stringify(resultState))
+            return resultState;
+        };
         case REMOVE_FROM_CART: {
             var index = -1;
             state.map((item, i) => {
-                if(action.payload.id === item.id) {
+                if (action.payload.id === item.id) {
                     index = i;
                 }
             });
             if (index > -1) {
                 state.splice(index, 1);
-                return [...state];
             }
+
+            const resultState = [...state];
+            localStorage.setItem(key, JSON.stringify(resultState))
+            return resultState;
         };
 
         default: return state;
